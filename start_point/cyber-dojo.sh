cc061dc9357789c73b47afdc3bc6b241d21e131a
JonJagger <jon@kosli.com>
2026-02-15 20:18:49 +0000

    Add ulimit command to see max-file size inside CI workflow run

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -18,13 +18,13 @@ function cyber_dojo_exit()
 cyber_dojo_enter
 trap cyber_dojo_exit EXIT SIGTERM
 
+ulimit -c # Prints 0
 ls -la 
-dotnet restore --source /home/sandbox/.nuget/packages/
-dotnet test --no-restore
-ls -al
+dotnet restore --source /home/sandbox/.nuget/packages/ --verbosity=diagnostic
+#dotnet test --no-restore
 
-# On CI workflow you get...
+# On MacBook I get...
 # "./cyber-dojo.sh: line 22:    14 File size limit exceeded(core dumped) dotnet restore --source /home/sandbox/.nuget/packages/\n",
 # "./cyber-dojo.sh: line 23:    16 File size limit exceeded(core dumped) dotnet test --no-restore\n"
-
-
+# This might be useful...
+# https://stackoverflow.com/questions/70469973/how-to-generate-coredump-file-in-alpine-container
